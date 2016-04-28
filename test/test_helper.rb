9edde75e49365b5e62152487a8fd5f992ca46fae
toshiotm <toshi.otm@gmail.com>
2016-04-28 13:19:53 +0900

    first commit

---
+++ test/test_helper.rb
@@ -0,0 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+
+require "capybara/rails"
+
+class ActionDispatch::IntegrationTest
+    include Capybara::DSL
+end
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
